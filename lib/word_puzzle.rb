a6a357ebec62b14a15d3eaf224fb637d40634fdb
steven francisco <stvnfrancisco@yahoo.com>
2015-06-02 16:25:19 -0700

    fixes merge conflict

--- lib/word_puzzle.rb
+++ lib/word_puzzle.rb
@@ -5,13 +5,24 @@ class String
     letters.each() do |letter|
     if letter == "a"
       final_sentence.push("-")
+    elsif
+      letter == "A"
+      final_sentence.push("-")
     elsif letter == "e"
+      final_sentence.push("-")
+    elsif letter == "E"
       final_sentence.push("-")
     elsif letter == "i"
       final_sentence.push("-")
+    elsif letter == "I"
+      final_sentence.push("-")
     elsif letter == "o"
       final_sentence.push("-")
+    elsif letter == "O"
+      final_sentence.push("-")
     elsif letter == "u"
+      final_sentence.push("-")
+    elsif letter == "U" 
       final_sentence.push("-")
     else
       final_sentence.push(letter)
